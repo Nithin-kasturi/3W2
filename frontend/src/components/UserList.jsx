d82ebb5cbec8d59d51100d5a0b5449256bdc9af0
Nithin-kasturi <nithinkasturi8@gmail.com>
2024-09-18 16:56:48 +0530

    Added select

--- frontend/src/components/UserList.jsx
+++ frontend/src/components/UserList.jsx
@@ -3,14 +3,17 @@ import { Form } from 'react-bootstrap';
 
 const UserList = ({ users, selectedUser, setSelectedUser }) => {
   return (
-    <Form.Group controlId="userSelect" >
+    <Form.Group controlId="userSelect">
       <h1>Select User and click claim button</h1>
       <Form.Control
-      className='border border-primary'
+        className="border border-primary"
         as="select"
         value={selectedUser}
         onChange={(e) => setSelectedUser(e.target.value)}
       >
+        <option value="" disabled>
+          Select
+        </option>
         {users.map((user) => (
           <option key={user._id} value={user._id}>
             {user.name}
