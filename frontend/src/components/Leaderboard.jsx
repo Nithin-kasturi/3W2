51c57c26936be733085a8ed3b8946f89fc2df2b6
Nithin-kasturi <nithinkasturi8@gmail.com>
2024-09-18 18:48:02 +0530

    added spinner

--- frontend/src/components/Leaderboard.jsx
+++ frontend/src/components/Leaderboard.jsx
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import { Table } from 'react-bootstrap';
+import { Button, Table, Spinner } from 'react-bootstrap';
 import { getLeaderboard } from '../api';
 
-const Leaderboard = ({selectedUser}) => {
+const Leaderboard = ({ selectedUser }) => {
   const [leaderboard, setLeaderboard] = useState([]);
-    const [currentName,setCurrentName]=useState("");
+  const [isLoading, setIsLoading] = useState(true); // New state for loading
+  const [currentName, setCurrentName] = useState('');
+
   useEffect(() => {
     const fetchLeaderboard = async () => {
       try {
         const response = await getLeaderboard();
         console.log(response.data);
         setLeaderboard(response.data);
+        setIsLoading(false); // Set loading to false after data is fetched
       } catch (error) {
         console.error('Error fetching leaderboard:', error);
+        setIsLoading(false); // Also stop loading if an error occurs
       }
     };
 
@@ -24,26 +28,34 @@ const Leaderboard = ({selectedUser}) => {
   return (
     <div className=''>
       <h2 className='text-center'>Leaderboard</h2>
-      <div className='table-responsive'>
-        <Table striped bordered hover className='w-100'>
-          <thead>
-            <tr>
-              <th>Rank</th>
-              <th>Name</th>
-              <th>Points</th>
-            </tr>
-          </thead>
-          <tbody>
-            {leaderboard.map((user, index) => (
-              <tr key={user._id}>
-                <td>{index + 1}</td>
-                <td>{user.name}</td>
-                <td>{user.points}</td>
+      {isLoading ? ( // Display spinner while loading
+        <div className="text-center">
+          <Spinner animation="border" role="status">
+            <span className="sr-only"></span>
+          </Spinner>
+        </div>
+      ) : (
+        <div className='table-responsive'>
+          <Table striped bordered hover className='w-100'>
+            <thead>
+              <tr>
+                <th>Rank</th>
+                <th>Name</th>
+                <th>Points</th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
-      </div>
+            </thead>
+            <tbody>
+              {leaderboard.map((user, index) => (
+                <tr key={user._id}>
+                  <td>{index + 1}</td>
+                  <td>{user.name}</td>
+                  <td>{user.points}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </div>
+      )}
     </div>
   );
 };
