43448b6e202f0a5989b86ee2fb4797880f2e5dc7
Nithin-kasturi <107863285+Nithin-kasturi@users.noreply.github.com>
2024-09-18 16:22:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,9 @@ app.use(cors(
 }
 ))
 app.use('/api/users', userRoutes);
-
+app.get('/',(req,res)=>{
+    res.send("Backend is working")
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
