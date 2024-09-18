f8843ff10d1cfb67b5e8d3b70e136feaf6a59ece
Nithin-kasturi <107863285+Nithin-kasturi@users.noreply.github.com>
2024-09-18 16:39:39 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import UserList from './components/userList';
+import UserList from './components/UserList';
 import ClaimButton from './components/ClaimButton';
 import Leaderboard from './components/Leaderboard';
 import axios from 'axios';
